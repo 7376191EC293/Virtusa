a5573ed90609b61f712766654ce66af482c5a110
Susmitha_Thinakaran <113764670+7376191EC293@users.noreply.github.com>
2022-11-23 22:56:10 +0530

    Add files via upload

---
+++ Tasks/Task-1 Dropdown/src/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dropdown';
+  picture = ['apple','mango','grapes','pineapple']
+  pic:any
+}
